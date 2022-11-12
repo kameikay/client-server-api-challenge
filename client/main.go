23df329762553adea074f5c08c9b295ec97e01a0
Victor Kamei Kay <kameikay@gmail.com>
2022-11-12 08:55:39 -0300

    add context to db

--- client/main.go
+++ client/main.go
@@ -1,3 +1,71 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Moeda struct {
+	Usdbrl struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
+
+const MAX_DURATION = time.Duration(time.Millisecond * 300)
+const URL = "http://localhost:8080/cotacao"
+
+func main() {
+	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), MAX_DURATION)
+	defer cancel()
+
+	req = req.WithContext(ctx)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	r, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var data Moeda
+	err = json.Unmarshal(r, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("Dólar: %s\n", data.Usdbrl.Bid))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao escrever no arquivo: %v\n", err)
+	}
+
+}
